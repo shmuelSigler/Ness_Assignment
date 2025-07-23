1c3d2210bba1568ce6dbaf0985068868cc38d76e
shmuelSigler <yakovsig@gmail.com>
2025-07-23 21:42:50 +0300

    increase code coverage %

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -9,4 +9,14 @@ public class UnitTest1
     {
         Assert.Equal("Hello World", Program.Print());
     }
+
+    [Fact]
+    public void Main_PrintsHelloWorldToConsole()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        Program.Main(Array.Empty<string>());
+        var consoleOutput = output.ToString().Trim();
+        Assert.Equal("Hello World", consoleOutput);
+    }
 }
